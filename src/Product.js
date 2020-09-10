5decd36a767fbfd7994178552cc101f6bc59385d
llamousse <vyue013@gmail.com>
2020-09-10 00:23:38 -0700

    styled and added product component

--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
-        <div className="productInfo">
-            <p>machine x</p>
-            <p className="productPrice">
-                <small>$</small>
-                <strong>19.99</strong>
-            </p>
-            <div className="productRating">
-
-            </div>
+      <div className="productInfo">
+        <p>{title}</p>
+        <p className="productPrice">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="productRating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
+      </div>
+
+      <img src={image} alt="" />
+
+      <button>Add to Basket</button>
     </div>
   );
 }
