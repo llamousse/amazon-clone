10225a822b54032dd144f24ec8f1d86c9b6db116
llamousse <vyue013@gmail.com>
2020-09-15 00:25:09 -0700

    basket dynamic update, subtotal num connected

--- src/reducer.js
+++ src/reducer.js
@@ -3,15 +3,16 @@ export const initialState = {
 };
 
 const reducer = (state, action) => {
+  console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
-    
+
     default:
-        return state;
+      return state;
   }
 };
 
