505d3ceea5d56a4d7066a70ea3c0d309d2e3b824
llamousse <vyue013@gmail.com>
2020-09-15 17:23:33 -0700

    styled login page

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    // firebase login stuff here
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    // firebase register here
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -11,6 +26,39 @@ function Login() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
         />
       </Link>
+
+      <div className="loginContainer">
+        <h1>Sign In</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit" onClick={signIn} className="loginSignInButton">
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By continuing, you agree to Amazon Clone's Conditions of Use and
+          Privacy Notice.
+        </p>
+
+        <button onClick={register} className="loginRegisterButton">
+          Create your Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
