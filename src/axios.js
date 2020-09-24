51df299cf88c70d9ae9034c849bf45086d2c2dd2
llamousse <vyue013@gmail.com>
2020-09-24 01:00:07 -0700

    replaced firebase api db link

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-d140e/us-central1/api", // the API URL (cloud function)
+  baseURL: "https://us-central1-clone-d140e.cloudfunctions.net/api", // the API URL (cloud function)
+  // http://localhost:5001/clone-d140e/us-central1/api
 });
 
 export default instance;
